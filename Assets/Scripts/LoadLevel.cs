ffa7f92418d594984ec4262a6d9ec926c8a2a528
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-07-26 20:01:31 +1000

    Menu scene created, all buttons load correct scenes

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartButton()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void SettingsButton()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void HowToPlay()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Bye bye");
+        Application.Quit();
+    }
+
+}
