8bd85f2b6cc4b687a60941e775ee6a089b9121e8
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-07-26 21:00:58 +1000

    Player health script created, my own scene for UI made with Health and no. of balloons. Scripts to be worked on.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public List<GameObject> healthImages;
+    public int maxHealth;
+    public int currentHealth;
+
+
+	// Use this for initialization
+	void Start () {
+        maxHealth = healthImages.Count;
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
