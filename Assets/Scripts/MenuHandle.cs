27a42219696c9bd85b7e1d9d7bb8e7a2fc6927ef
DESKTOP-QJ28QB9\The Jman <carrots11@y7mail.com>
2017-08-01 21:03:24 +1000

    Changed the load level script to Menu handle, buttons now toggle between using the up and down arrow keys. Colour was also added to them while highlighted.

---
+++ Assets/Scripts/MenuHandle.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuHandle : MonoBehaviour {
+
+    public GameObject [] buttons;
+    public string [] SceneName;
+    public Color UnSelected;
+    public int Selected;
+
+	// When menu loads it is given it's designated colour
+	void Start () {
+        UnSelected = buttons[0].GetComponent<Image>().color;
+        buttons[0].GetComponent<Image>().color = (Color.green);	
+	}
+	
+	// Allows movement through menu with arrow keys
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Selected >= 1)
+            {
+                Selected -= 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("verticalP1") == -1f)
+        {
+            if (Selected < buttons.Length - 1)
+            {
+                Selected += 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (Selected < buttons.Length - 1))
+        {
+            SceneManager.LoadScene(SceneName[Selected]);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (Selected == buttons.Length - 1))
+        {
+            Debug.Log("Is quitting");
+            QuitGame();
+        }
+
+        if (Selected == 0)
+        {
+            StartButton();
+        }
+        else
+        {
+            buttons[0].GetComponent<Image>().color = (UnSelected);
+        }
+        if (Selected == 1)
+        {
+            SettingsButton();
+        }
+        else
+        {
+            buttons[1].GetComponent<Image>().color = (UnSelected);
+        }
+        if (Selected == 2)
+        {
+            HowToPlay();
+        }
+        else
+        {
+            buttons[2].GetComponent<Image>().color = (UnSelected);
+        }
+        if (Selected == 3)
+        {
+            Credits();
+        }
+        else
+        {
+            buttons[3].GetComponent<Image>().color = (UnSelected);
+        }
+        if (Selected == 4)
+        {
+            QuitGame();
+        }
+        else
+        {
+            buttons[4].GetComponent<Image>().color = (UnSelected);
+        }
+    }
+
+    public void StartButton()
+    {
+        buttons[0].GetComponent<Image>().color = (Color.green);
+    }
+
+    public void SettingsButton()
+    {
+        buttons[1].GetComponent<Image>().color = (Color.green);
+    }
+
+    public void HowToPlay()
+    {
+        buttons[2].GetComponent<Image>().color = (Color.green);
+    }
+
+    public void Credits()
+    {
+        buttons[3].GetComponent<Image>().color = (Color.green);
+    }
+
+    public void QuitGame()
+    {
+        buttons[4].GetComponent<Image>().color = (Color.green);
+    }
+
+}
