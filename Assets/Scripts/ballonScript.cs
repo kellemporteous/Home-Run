ce8fedd87ba19f4c831084224870919b6b85e3e3
DESKTOP-S75MN06\user <1000417@student.sae.edu.au>
2017-08-02 17:28:10 +1000

    fixed

--- Assets/Scripts/ballonScript.cs
+++ Assets/Scripts/ballonScript.cs
@@ -5,26 +5,54 @@ using UnityEngine;
 public class ballonScript : MonoBehaviour {
 
     public float speed;
+    public string direction;
+    public bool left;
+    public GameObject player;
+   
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+		left = player.GetComponent<SpriteRenderer>().flipX;
+        if (left)
+        {
+           
+        }
+        else
+        {
+            direction = "+";
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = new Vector2(transform.position.x + speed / 100, transform.position.y);
-	}
+        if (left)
+        {
+            transform.position = new Vector2(transform.position.x - speed / 100, transform.position.y);
+        }
+
+        if (!left)
+        {
+            transform.position = new Vector2(transform.position.x + speed / 100, transform.position.y);
+        }
+    }
+       
 
     public void OnTriggerEnter2D(Collider2D other)
 
     {
-        if (other.gameObject.tag == ("Enemy"))
+        if (other.gameObject.tag == ("Enemy") )
+        {
+            
+            other.gameObject.GetComponent<Enemy>().health -= 1;
+            Destroy(gameObject);
+        }
+        if (other.gameObject.tag == ("Enemy") )
         {
             other.gameObject.GetComponent<Enemy>().health -= 1;
             Destroy(gameObject);
         }
 
-        
     }
 }
