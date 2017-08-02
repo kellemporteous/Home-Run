4e9c7f1f70cfeb3c6afb6e263c7078031d2af3e0
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-08-02 14:56:06 +1000

    Analytics was added into player health.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -10,22 +10,29 @@ public class PlayerHealth : MonoBehaviour {
     public int currentHealth;
     public int damage;
     public GameObject spawner;
-
+	public float startTime;
 
 	// Use this for initialization
 	void Start () {
         maxHealth = healthImages.Count;
         currentHealth = maxHealth;
         spawner = GameObject.FindGameObjectWithTag("spawner");
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (currentHealth <= 0)
         {
-
+			Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+				{
+					{ "playTime", Time.time-startTime }
+					//{ "coins", totalCoins },
+					//{ "activeWeapon", weaponID }
+				});
             spawner.GetComponent<playerdist>().spawning = true;
             currentHealth = maxHealth;
+			startTime = Time.time;
         }
 	}
 }
