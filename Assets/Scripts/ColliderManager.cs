ac34d52c8e2d46e9e5700e6f1595e1e561b456cb
DESKTOP-S75MN06\user <1000417@student.sae.edu.au>
2017-08-04 09:42:41 +1000

    updated enemy can now be killed with ballons and UI is added to main sceene

--- Assets/Scripts/ColliderManager.cs
+++ Assets/Scripts/ColliderManager.cs
@@ -39,7 +39,7 @@ public class ColliderManager : MonoBehaviour {
         spritesList.Add(sprite);
         int index = spritesList.IndexOf(sprite);
         PolygonCollider2D spriteCollider = gameObject.AddComponent<PolygonCollider2D>();
-        spriteCollider.isTrigger = isATrigger;
+        spriteCollider.isTrigger = true;
 
         spriteColliders.Add(index, spriteCollider);
         yield return new WaitForEndOfFrame();
@@ -66,6 +66,10 @@ public class ColliderManager : MonoBehaviour {
         spriteColliders = new Dictionary<int, PolygonCollider2D>();
 
         Frame = spritesList.IndexOf(spriteRenderer.sprite);
+
+        Rigidbody2D ridgbody = gameObject.AddComponent<Rigidbody2D>();
+        if (ridgbody != null)
+        ridgbody.isKinematic = true;
     }
 
     private void LateUpdate()
