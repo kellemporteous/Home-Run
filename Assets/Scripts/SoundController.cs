1502129c7eabfa0d58c839b36d644e8a7225557a
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-08-07 11:29:20 +1000

    Sound controller script added

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+    public static SoundController instance;
+
+    AudioSource audioSource;
+
+    public AudioClip P1Slap;
+    public AudioClip P2Slap;
+    public AudioClip BalloonPickup;
+    public AudioClip BalloonThrow;
+    public AudioClip EnemySlap;
+    public AudioClip P1Jump;
+    public AudioClip P2Jump;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
