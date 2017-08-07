f697efb89d8a1f1389cbf6cf350a69b9111fb70d
DESKTOP-S75MN06\user <1000417@student.sae.edu.au>
2017-08-08 08:55:37 +1000

    camera switchs to two player mode if second player activated

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -8,16 +8,22 @@ public class CameraScript : MonoBehaviour
     public float offset;
     public float lerpspeed;
     public GameObject player;
+    public GameObject player2;
+    public GameObject twoplayerObj;
     // Use this for initialization
     void Start ()
     {
         cameraOBJ = GameObject.FindGameObjectWithTag("MainCamera");
-        
+       
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (player2.activeInHierarchy)
+        {
+            player = twoplayerObj;
+        }
         Vector3 move = Vector3.MoveTowards(cameraOBJ.transform.position, player.transform.position * offset, Time.deltaTime * lerpspeed);
         move.y = cameraOBJ.transform.position.y;
         move.z = cameraOBJ.transform.position.z;
