37f8a61b6653aed096d530da4635a08447caa24c
DESKTOP-QJ28QB9\The Jman <carrots11@y7mail.com>
2017-08-09 12:03:36 +1000

    Sound controller script is completed. Need to implement the sounds into the other scripts to have them work

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -10,19 +10,55 @@ public class SoundController : MonoBehaviour {
 
     public AudioClip P1Slap;
     public AudioClip P2Slap;
-    public AudioClip BalloonPickup;
-    public AudioClip BalloonThrow;
-    public AudioClip EnemySlap;
+    public AudioClip balloonPickup;
+    public AudioClip balloonThrow;
+    public AudioClip enemySlap;
     public AudioClip P1Jump;
     public AudioClip P2Jump;
 
 	// Use this for initialization
 	void Start () {
-		
+        instance = this;
+        audioSource = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void P1slap()
+    {
+        audioSource.PlayOneShot(P1Slap);
+    }
+
+    public void P2slap()
+    {
+        audioSource.PlayOneShot(P2Slap);
+    }
+
+    public void BalloonPickup()
+    {
+        audioSource.PlayOneShot(balloonPickup);
+    }
+
+    public void BalloonThrow()
+    {
+        audioSource.PlayOneShot(balloonThrow);
+    }
+
+    public void EnemySlap()
+    {
+        audioSource.PlayOneShot(enemySlap);
+    }
+
+    public void P1jump()
+    {
+        audioSource.PlayOneShot(P1Jump);
+    }
+
+    public void P2jump()
+    {
+        audioSource.PlayOneShot(P2Jump);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
